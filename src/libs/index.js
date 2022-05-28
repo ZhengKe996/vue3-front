4ed5e4afb8020ef20e0f0d56b7e3518c0cd66935
ZhengKe <3119531802@qq.com>
2022-05-28 19:53:51 +0800

    feat: 自定义组件 自动注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,7 +1,15 @@
-import SvgIcon from "./svg-icon";
+import { defineAsyncComponent } from "vue";
 
 export default {
   install(app) {
-    app.component("m-svg-icon", SvgIcon);
+    // 1. 获取当前路径下所有文件夹中的 index.vue
+    const components = import.meta.glob("./*/index.vue");
+
+    // 2. 遍历获取到的组件模块
+    for (const [fullPath, fn] of Object.entries(components)) {
+      // 3. 利用 app.component 进行注册
+      const componentName = `m-${fullPath.replace("./", "").split("/")[0]}`;
+      app.component(componentName, defineAsyncComponent(fn));
+    }
   },
 };
