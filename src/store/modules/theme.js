6d720a5f160c65f9ac16c17728e856b96df8f12c
ZhengKe <3119531802@qq.com>
2022-06-09 20:27:07 +0800

    feat: 跟随系统主题

--- src/store/modules/theme.js
+++ src/store/modules/theme.js
@@ -3,7 +3,7 @@ import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from "@/constants";
 export default {
   namespaced: true,
   state: () => ({
-    themeType: THEME_DARK,
+    themeType: THEME_LIGHT,
   }),
   mutations: {
     changeThemeType(state, newTheme) {
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,25 +3,46 @@ import store from "../store";
 import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from "@/constants";
 
 /**
+ * 监听系统主题变更
+ */
+let matchMedia;
+const watchSystemThemeChange = () => {
+  if (matchMedia) return;
+
+  matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
+  matchMedia.onchange = () => {
+    changeTheme(THEME_SYSTEM);
+  };
+};
+
+const changeTheme = (theme) => {
+  // html 的 class
+  let themeClassName = "";
+
+  switch (theme) {
+    case THEME_LIGHT:
+      themeClassName = "light";
+      break;
+    case THEME_DARK:
+      themeClassName = "dark";
+    case THEME_SYSTEM:
+      watchSystemThemeChange();
+      themeClassName = matchMedia.matches ? "dark" : "light";
+      break;
+  }
+
+  // 修改 html 的 class
+  document.querySelector("html").className = themeClassName;
+};
+
+/**
  * 初始化主题
  */
 export default () => {
   watch(
     () => store.getters.themeType,
     (value) => {
-      // html 的 class
-      let themeClassName = "";
-
-      switch (value) {
-        case THEME_LIGHT:
-          themeClassName = "light";
-          break;
-        case THEME_DARK:
-          themeClassName = "dark";
-      }
-
-      // 修改 html 的 class
-      document.querySelector("html").className = themeClassName;
+      changeTheme(value);
     },
     { immediate: true }
   );
